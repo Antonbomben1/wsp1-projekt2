bf890281e8c1aa280c39ac374798be859672a3c2
Anton Angervall <anton.angervall@elev.ga.ntig.se>
2024-11-18 14:48:09 +0100

    fixat med knapparna
    undo knapp, samt en tabell för id, username,password

--- app.rb
+++ app.rb
@@ -1,7 +1,37 @@
 class App < Sinatra::Base
+    def db
+        return @db if @db
 
-    get '/' do
-        erb(:"index")
+        @db = SQLite3::Database.new("db/todo.sqlite")
+        @db.results_as_hash = true
+
+        return @db
     end
 
+    get '/' do
+        @todo = db.execute("SELECT * FROM todo")
+        erb :index
+      end
+    
+    post '/todo' do
+        name = params[:name]
+        description = params[:description]
+        db.execute("INSERT INTO todo (name, description) VALUES (?,?)", [name, description])
+        redirect '/'
+      end
+    
+    post '/todo/:id/delete' do
+        db.execute("DELETE FROM todo WHERE id = ?", [params[:id]])
+        redirect '/'
+      end
+
+
+    post '/todo/:id/:done' do |id, done|
+        if done == "1"
+            db.execute("UPDATE todo SET done = 0 WHERE id = ?", [params[:id]])
+        elsif done == "0"
+            db.execute("UPDATE todo SET done = 1 WHERE id = ?", [params[:id]])
+        end
+        redirect '/'
+      end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,4 +1,5 @@
 require 'sqlite3'
+require 'bcrypt'
 
 class Seeder
 
@@ -10,20 +11,31 @@ class Seeder
 
   def self.drop_tables
     db.execute('DROP TABLE IF EXISTS todo')
+    db.execute('DROP TABLE IF EXISTS users')
   end
+
   def self.create_tables
     db.execute('CREATE TABLE todo (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                description TEXT)')
+                name TEXT,
+                description TEXT,
+                done BOOLEAN DEFAULT FALSE)');
+
+    db.execute('CREATE TABLE users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT,
+                password TEXT)');
   end
 
-  def self.populate_tables 
-    db.execute('INSERT INTO todo (description) VALUES ("Buy groceries")')
-    db.execute('INSERT INTO todo (description) VALUES ("Mow the lawn")')
-    db.execute('INSERT INTO todo (description) VALUES ("Go to the gym")')
-    db.execute('INSERT INTO todo (description) VALUES ("Go to the gym2")')
-    #db.execute('INSERT INTO fruits (name, tastiness, description, color) VALUES ("Banan",  4, "En avlång frukt." , "Gul 🟡")')
+  
 
+  def self.populate_tables 
+    db.execute('INSERT INTO todo (name, description) VALUES ("Buy groceries", "Milk, eggs, butter")')
+    db.execute('INSERT INTO todo (name, description) VALUES ("Mow the lawn", "Backyard")')  
+    db.execute('INSERT INTO todo (name, description) VALUES ("Go to the gym", "Leg day")')
+
+    password_hashed = BCrypt::Password.create("admin")
+    db.execute('INSERT INTO users (username, password) VALUES ("admin", ?)', [password_hashed])
   end
 
   def self.db
